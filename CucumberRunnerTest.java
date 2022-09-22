0cf943e4186fd287e5bb392bec7a8f9dbbeb00e0
bhumichouhan <bhumi.wj@gmail.com>
2022-09-22 08:43:40 -0500

    changed tag by Bhumika

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/main/resources/features",
         glue = "stepDefintion"
         Aparna
-//       tags = "@TagChangebyBhumika",
+//       tags = "@Bhumika",
 //        tags = "@by AparnaD",
  master
 //        plugin = {"pretty","html:target/report/cucumber.html",
