daa66ace9cf0defc680f3a0f023f94a583ed6ec1
sudhatalluri <sudha.talluri@gmail.com>
2022-09-22 17:25:05 -0500

    updated tag name to sudha

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/main/resources/features",
         glue = "stepDefintion"
-//        tags = "@Regression",
+//        tags = "@sudha",
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 
