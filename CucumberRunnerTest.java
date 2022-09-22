809b83bac378429b62afeb57d3179c698a3a615a
Valli <vallisenthil_s@yahoo.com>
2022-09-22 13:08:21 -0500

    updated from jayasri to valli

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/main/resources/features",
         glue = "stepDefintion"
-//        tags = "@JayasriJetti",
+//        tags = "@Valli",
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 
