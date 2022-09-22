88e3c8ab1168b96a6ed2e6e0a8d29d8a8b50c147
Kalyan <be.kalyankumar@gmail.com>
2022-09-21 19:49:26 -0500

    Added CucumberRunner.java

---
+++ CucumberRunnerTest.java
@@ -0,0 +1,17 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/main/resources/features",
+        glue = "stepDefintion"
+//        tags = "@Smoke",
+//        plugin = {"pretty","html:target/report/cucumber.html",
+//                "json:target/cucumber.json"}
+
+//        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"},
+//        plugin = {"pretty","io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"}
+)
+public class CucumberRunnerTest extends AbstractTestNGCucumberTests
+{
+
+}
