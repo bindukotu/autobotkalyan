ef278dc124b8472e3012ad4b2be514506767886b
umagithub7 <114123624+umagithub7@users.noreply.github.com>
2022-09-23 18:03:39 -0400

    sri changes

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/main/resources/features",
         glue = "stepDefintion"
-//        tags = "@Sridevi",
+//        tags = "@for Sridevi",
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 
