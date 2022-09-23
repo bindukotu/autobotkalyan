89ce0e2fa7a6b10eb2f47feeaed6b2f03422258b
AparnaDarapaneni <aparnadarapaneni@gmail.com>
2022-09-23 00:49:25 -0500

    changedAppD

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/main/resources/features",
         glue = "stepDefintion"
-//        tags = "@Lucky1",
+//        tags = "@Aparna22",
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 
