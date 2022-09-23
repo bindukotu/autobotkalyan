fae330d95c660ca138e6d694b41f43aafb3c88d6
Valli <vallisenthil_s@yahoo.com>
2022-09-23 14:22:59 -0500

    changed the tag to Val

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/main/resources/features",
         glue = "stepDefintion"
-//        tags = "@Aparna22",
+//        tags = "@Val",
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 
