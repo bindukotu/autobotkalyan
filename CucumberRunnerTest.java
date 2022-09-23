0a5815645e13266363f09304a705320864c76689
Kalyan <be.kalyankumar@gmail.com>
2022-09-22 19:18:57 -0500

    Revert "Revert "updated""
    This reverts commit 76ac647b8b7840300bfc472e0f80e79432bebd77.

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/main/resources/features",
         glue = "stepDefintion"
-//        tags = "@Regression",
+//        tags = "@Regression1",
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 
