295442248dca6f8eb42b5339f486b19e6309a7b9
Hina Shakeel <hina.engr87@gmail.com>
2022-09-24 12:08:06 -0500

    changed to 1

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/main/resources/features",
         glue = "stepDefintion"
 
-//        tags = "@Hoor",
+//        tags = "@Hoor1",
 
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
