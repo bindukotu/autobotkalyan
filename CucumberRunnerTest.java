eac047eb3844ae606821d44f94ada3d5dc2f003a
Muhammad Ebad Ullah <ullahmu@txslmacl3599.vzwcorp.com>
2022-09-24 00:04:59 -0500

    ray added

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/main/resources/features",
         glue = "stepDefintion"
-//        tags = "@TagChangedByHina",
+//        tags = "@RAY",
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 
