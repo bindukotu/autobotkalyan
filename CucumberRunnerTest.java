8050ccc0c8609eba75348f30436f22304fe3752e
perni-chandini996 <pernichandini@gmail.com>
2022-09-24 12:09:05 -0500

    testing

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/main/resources/features",
         glue = "stepDefintion"
-//        tags = "@chandini",
+//        tags = "@chandini1",
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 
