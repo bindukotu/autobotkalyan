613979f5c5164e8b76249406f274ffd364f89258
umagithub7 <sridevi18.ch@gmail.com>
2022-09-29 14:59:56 -0400

    making the changes from SinduY to Annu

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/main/resources/features",
         glue = "stepDefintion"
 
-//        tags = "@sinduy",
+//        tags = "@Annu_sridevi making the update",
 
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
