07169349e1c3f8e1aed0f26631f03d0786a7dd61
umagithub7 <sridevi18.ch@gmail.com>
2022-10-01 19:49:02 -0400

    changed

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -3,16 +3,9 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/main/resources/features",
-<<<<<<< HEAD
-        glue = "stepDefintion"
-
-//       tags = "@aruna",
-//   plugin = {"pretty","html:target/report/cucumber.html",
-=======
         glue = "Sridevi is making these changes"
 //        tags = "@Changed by Sridevi",
 //        plugin = {"pretty","html:target/report/cucumber.html",
->>>>>>> master
 //                "json:target/cucumber.json"}
 
 //        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"},
