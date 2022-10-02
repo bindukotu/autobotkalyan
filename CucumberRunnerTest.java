eaffa827eeee196f382478130e32f98f98941f19
umagithub7 <sridevi18.ch@gmail.com>
2022-10-02 14:15:22 -0400

    current changes made by Sridevi

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-giimport io.cucumber.testng.AbstractTestNGCucumberTests;
-=======
+
 gitimport io.cucumber.testng.AbstractTestNGCucumberTests;
->>>>>>> 0bb2b06a6040733d0233b2f80b4cf49c5637db0f
+
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-        features = "src/main/resources/features",
-<<<<<<< HEAD
         glue = "stepDefintion"
-
 //        tags = "@Changes made by Sridevi",
-
-=======
         glue = "Sridevi is making these changes again"
 //        tags = "@Changed by Sridevich",
->>>>>>> 0bb2b06a6040733d0233b2f80b4cf49c5637db0f
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 
