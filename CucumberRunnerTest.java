93211be46858610f64c04cb7b81f7aacd45d17d8
sailaja siripurapu <s.j.sailaja@gmail.com>
2022-10-09 17:35:52 -0700

    Updated

--- CucumberRunnerTest.java
+++ CucumberRunnerTest.java
@@ -4,12 +4,8 @@ gitimport io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
         glue = "stepDefintion"
-<<<<<<< HEAD
+
 //        tags = "@Sailaja1",
-=======
-//       tags = "@Changes are for Sri",
-//       tags = "@Changes made by Lakshmi",
->>>>>>> 0d71dd475dc9edfb3fa74ae65181aacdfe090671
 //        plugin = {"pretty","html:target/report/cucumber.html",
 //                "json:target/cucumber.json"}
 //        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"},
