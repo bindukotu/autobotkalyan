25118852321b8cb4894f47d26a8c65e326ddc43e
Yamuna2022 <yglearn2022@gmail.com>
2022-10-31 22:42:23 -0400

    YamunaUnitTests

--- Pet_Store1/src/main/java/YamunaUnitTests.java
+++ Pet_Store1/src/main/java/YamunaUnitTests.java
@@ -2,9 +2,13 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.given;
 
-public class Sailaja_UnitTests {
+public class YamunaUnitTests {
+
+
+
     @Test(priority = 1)
     public void getCall(){
         RestAssured.baseURI="https://reqres.in/";
@@ -13,8 +17,8 @@ public class Sailaja_UnitTests {
                 header("Content-type","application/json").
                 get("api/users/2").andReturn();
         int statusCode=response.getStatusCode();
+        System.out.println("In Get Call");
         Assert.assertEquals(statusCode,200);
-
     }
     @Test(priority = 2)
     public void postCall(){
@@ -26,6 +30,7 @@ public class Sailaja_UnitTests {
                         "    \"name\": \"morpheus\",\n" +
                         "    \"job\": \"leader\"\n" +
                         "}").post("api/users");
+        System.out.println("In post Call");
         int statusCode=response.getStatusCode();
         Assert.assertEquals(statusCode,201);
     }
@@ -39,9 +44,9 @@ public class Sailaja_UnitTests {
                         "    \"name\": \"morpheus\",\n" +
                         "    \"job\": \"zion resident\"\n" +
                         "}").put("api/users/2");
+        System.out.println("In put Call");
         int statusCode=response.getStatusCode();
         Assert.assertEquals(statusCode,200);
-
     }
     @Test(priority = 4)
     public void deleteCall(){
@@ -50,11 +55,9 @@ public class Sailaja_UnitTests {
                 when().
                 header("Content-type","application/json").
                 delete("api/users/2").andReturn();
+        System.out.println("In Delete Call");
         int statusCode=response.getStatusCode();
         Assert.assertEquals(statusCode,204);
-
-
     }
-
-
 }
+
